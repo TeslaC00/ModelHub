ee2e5b7827a48fdf865f8c47272d212565e549f6
TeslaC00 <vikaskumar16122002@gmail.com>
2024-04-09 23:49:49 +0530

    moved secret key to .env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv, get_key
+
+# Load .env file values
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3$r93nud3p8!zj9nvl%-4qicru))(u8-@7=7+^8pc9r^dra05y'
+SECRET_KEY = get_key('.env','SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
